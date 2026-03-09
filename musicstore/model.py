f7515bed455fdb092ecd174bfddeb9837d7148f0
Juan Camilo Holguin <jholguin887@soyudemedellin.edu.co>
2026-03-09 18:44:10 -0500

    Se definió la clase Transaction y su método constructor

--- musicstore/model.py
+++ musicstore/model.py
@@ -1,3 +1,10 @@
 from datetime import datetime
 
-# TODO: Add code here
+class Transaction:
+    SELL: int = 1
+    SUPPLY: int = 2
+
+    def __init__(self, type: int, copies: int):
+        self.type: int = type
+        self.copies: int = copies
+        self.date: datetime = datetime.now()
